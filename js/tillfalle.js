92bc263f6381a49216187d81ffd8a3c896439354
itggot-joel-eriksson <joel.eriksson3@itggot.se>
2016-01-13 10:59:01 +0100

    Fixes #4

--- js/tillfalle.js
+++ js/tillfalle.js
@@ -1,65 +1,48 @@
 $(document).ready(function() {
-  /**
-   * Använd AJAX för att fråga servern efter filen benämnd TILLFALLE i föregående mapp
-   * relativt till denna javascript fil.
-   */
-  $.ajax({ url: "../TILLFALLE",
-    success: function(data) {
-      var regex = /\/\*([^*]|[\r\n])*\*\//mg;
-      data = data.replace(regex,'').trim(); // ta bort alla kommentarer från filen
-      if(data.length < 10 || !(/^(19|20)\d\d(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])([0-9]|0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/.test(data))) return; // testa om datumet är korrekt enligt formattet YmdHi
-      tillfalle = new Date(data.substr(0,4),+data.substr(4,2)-1,data.substr(6,2),data.substr(8,2),data.substr(10,2),0,0);
-    }
-  }).done(function() {
+    /*
+     * Definiera variabler med nyckelordet "var"
+     */
+    var tillfalle,
+        interval;
 
-    /**
-      * Om TILLFALLE var tom eller inte kunde läsas, sätt tillfalle till närmaste onsdag
-      * klockan tre, om klockan passerat fem sätts tillfalle till onsdag nästkommande vecka.
-      */
-    if(isNaN(tillfalle) || (tillfalle.getTime()-Date.now())<=7200000) {
-      tillfalle = new Date();
-      //tillfalle.setTime(tillfalle.getTime() + (259200000 - (86400000*tillfalle.getDay()) + 604800000) % 604800000);
-      tillfalle.setDate(tillfalle.getDate()+((tillfalle.getHours() < 17) ? ((3-tillfalle.getDay()+7)%7) : ((3-1-tillfalle.getDay()+7)%7+1)));
-      tillfalle.setHours(15);
-      tillfalle.setMinutes(0);
-      tillfalle.setSeconds(0);
-      tillfalle.setMilliseconds(0);
-    }
-  
-    /**
-      * Skriv det resulterande datumet till elementet med ID 'tillfalle' i formattet
-      * d F Y H:i, ex. 16 September 2015 15:00
-      */
-    var m = ['Januari','Februari','Mars','April','Maj','Juni','Juli',
-'Augusti','September','Oktober','November','December'];
-    $('#tillfalle').html(tillfalle.getDate() + ' ' + m[tillfalle.getMonth()] + ' ' + tillfalle.getFullYear() + ' ' + ('00'+tillfalle.getHours()).slice(-2) + ':' + ('00'+tillfalle.getMinutes()).slice(-2));
-
-    /**
-      * Räkna ner tiden tills tillfalle inträffar.
-      */
-    visning = document.querySelector('#tid')
-    if((tillfalle.getTime()-Date.now())<=0) {
-      visning.innerHTML = '<strong>PÅGÅR JUST NU</strong> till 17:00';
-    }else {
-      var tidtagare = new CountDownTimer((tillfalle-Date.now())/1000);
-      tidtagare.onTick(format).onTick(function() {
-        if(this.expired()) {
-          visning.innerHTML = '<strong>PÅGÅR JUST NU</strong> till 17:00';   
+    function done_fn() {
+        if (moment().isValid(data.custom.start.date + " " + data.custom.start.time) && moment().isValid(data.custom.end.date + " " + data.custom.end.time)) {
+            var start = moment(data.custom.start.date + " " + data.custom.start.time);
+            var end = moment(data.custom.end.date + " " + data.custom.end.time);
+            if (moment().isBefore(start) === true) { // tillfalle.json har datum som är i framtiden
+                $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
+                $("#tid").html(moment().to(start));
+            } else if (moment().isBetween(start, end)) { // tillfalle.json har datum som pågår nu
+                $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
+                end_time = (data.custom.start.date === data.custom.end.date) ? data.custom.end.time : end.format("D MMMM H:mm");
+                $("#tid").html("<strong>PÅGÅR JUST NU</strong> till " + end_time);
+            } else { // tillfalle.json har datum som är i dåtiden
+                $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
+                $("#tid").html(moment(moment().to(moment().weekday(2).format("YYYY-MM-DD")) + " " + data.default.start.time));
+            }
         }
-      }).start();
     }
-  });
 
-  /**
-    * tid till tillfälle
-    * NOTERA: detta är inte korrekt, om ett dygn exempelvis har mer än 86400 sekunder
-    * någon dag blir saker jobbigt!
-    */
-  function format(ttt) {
-    var days = ((ttt / 86400) | 0);
-    var hours = ((ttt / 3600) | 0) % 24;
-    var minutes = ((ttt / 60) | 0) % 60;
-    var seconds = ((ttt % 60) | 0);
-    visning.innerHTML = days + ' dagar, ' + ('00'+hours).slice(-2) + ':' + ('00'+minutes).slice(-2) + ':' + ('00'+seconds).slice(-2) + ' timmar';
-  }
+    /*
+     * Språket för moment.js är svenska
+     */
+    moment.locale("sv");
+    moment.relativeTimeThreshold("s", 59);
+    moment.relativeTimeThreshold("m", 59);
+    moment.relativeTimeThreshold("h", 23);
+
+    /*
+     * Använd AJAX för att fråga servern efter filen benämnd TILLFALLE i föregående mapp
+     * relativt till denna javascript fil.
+     */
+    $.ajax({
+        url: "../tillfalle.json",
+        success: function(json) {
+            data = $.parseJSON(json);
+        }
+    }).done(function() {
+        setInterval(function () {
+            done_fn();
+        }, 250);
+    });
 });
