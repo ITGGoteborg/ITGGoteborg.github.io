98c683e4cf97cd935812447fe52a6b753e8933b8
Joel Eriksson <joel.eriksson3@itggot.se>
2016-09-05 20:07:23 +0200

    Maintainance work
    - Update tillfalle.js to an easier method of solving #6
    - Update Font Awesome version
    - Patched logo anomaly
    - Updated favicon

--- js/tillfalle.js
+++ js/tillfalle.js
@@ -1,77 +1,57 @@
 $(document).ready(function() {
-    /*
-     * Definiera variabler med nyckelordet "var"
-     */
-    var tillfalle,
-        interval;
+	var tillfalle;
+	var interval;
 
-    function done_fn() {
-        var end, start;
-        if (moment().isValid(data.custom.start.date + " " + data.custom.start.time)
-            && moment().isValid(data.custom.end.date + " " + data.custom.end.time)
-            && moment().isBefore((end = moment(data.custom.end.date + " " + data.custom.end.time)))) {
-            start = moment(data.custom.start.date + " " + data.custom.start.time);
-            setInterval(function () {
-              if (moment().isBefore(start) === true) { // tillfalle.json har datum som är i framtiden
-                  $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
-                  $("#tid").html(moment().to(start));
-              } else if (moment().isBetween(start, end)) { // tillfalle.json har datum som pågår nu
-                  $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
-                  //end_time = (data.custom.start.date === data.custom.end.date) ? data.custom.end.time : end.format("D MMMM H:mm"); // vad är data.custom.end.date?
-                  end_time = (moment(end).diff(start, 'days') == 0) ? data.default.end.time : end.format("D MMMM H:mm");
-                  $("#tid").html("<strong>PÅGÅR JUST NU</strong> till " + end_time);
-              }
-            }, 1000);
-        }else {
-            var time = data.default.start.time;
-            start = moment().day(data.default.start.day);
+	function done_fn(data) {
+		var start;
+		var end;
 
-            time = time.split(':');
-            start.set({
-              hours: time[0],
-              minutes: time[1]
-            })
+		if (data.custom && moment().isValid(data.custom.start.date + " " + data.custom.start.time) && moment().isValid(data.custom.end.date + " " + data.custom.end.time) && moment().isBefore((end = moment(data.custom.end.date + " " + data.custom.end.time)))) {
+			setInterval(function () {
+				start = moment(data.custom.start.date + " " + data.custom.start.time);
+				
+				if (moment().isBefore(start) === true) { // tillfalle.json har datum som är i framtiden
+					$("#tillfalle").html(start.format("dddd D MMMM YYYY HH:mm"));
+					$("#tid").html(moment().to(start));
+				} else if (moment().isBetween(start, end)) { // tillfalle.json har datum som pågår nu
+					$("#tillfalle").html(start.format("dddd D MMMM YYYY HH:mm"));
+					end_time = (moment(end).diff(start, "days") === 0) ? data.default.end.time : end.format("dddd D MMMM HH:mm");
+					$("#tid").html("<strong>PÅGÅR JUST NU</strong> till " + end_time);
+				}
+			}, 1000);
+		} else {
+			setInterval(function () {
+				start = moment().isAfter(moment().set({ day: data.default.start.day, hour: data.default.end.hour, minute: data.default.end.minute, })) ? moment().set({ day: data.default.start.day + 7, hour: data.default.start.hour, minute: data.default.start.minute, }) : moment().set({ day: data.default.start.day, hour: data.default.start.hour, minute: data.default.start.minute, });
+				end = moment().isAfter(moment().set({ day: data.default.start.day, hour: data.default.end.hour, minute: data.default.end.minute, })) ? moment().set({ day: data.default.start.day + 7, hour: data.default.end.hour, minute: data.default.end.minute, }) : moment().set({ day: data.default.start.day, hour: data.default.end.hour, minute: data.default.end.minute, });
 
-            time = data.default.end.time;
-            var end = moment().day(data.default.end.day);
+				if (moment().isBetween(start, end)) {
+					$("#tillfalle").html("");
+					$("#tid").html("<strong>PÅGÅR JUST NU</strong> till " + end.format("HH:mm"));
+				} else {
+					$("#tillfalle").html(start.format("dddd D MMMM YYYY HH:mm"));
+					$("#tid").html(moment().to(start));
+				}
+			}, 1000);
+		}
+	}
 
-            time = time.split(':');
-            end.set({
-              hours: time[0],
-              minutes: time[1]
-            })
+	/**
+	 * Språket för moment.js är svenska
+	 */
+	moment.locale("sv");
+	moment.relativeTimeThreshold("s", 59);
+	moment.relativeTimeThreshold("m", 59);
+	moment.relativeTimeThreshold("h", 23);
 
-            setInterval(function () {
-              if (moment().isBefore(start) === true) { // tillfalle.json har datum som är i framtiden
-                  $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
-                  $("#tid").html(moment().to(start));
-              } else if (moment().isBetween(start, end)) { // tillfalle.json har datum som pågår nu
-                  $("#tillfalle").html(start.format("D MMMM YYYY H:mm"));
-                  end_time = (moment(end).diff(start, 'days') == 0) ? data.default.end.time : end.format("D MMMM H:mm");
-                  $("#tid").html("<strong>PÅGÅR JUST NU</strong> till " + end_time);
-              }
-            }, 1000);
-        }
-    }
-
-    /*
-     * Språket för moment.js är svenska
-     */
-    moment.locale("sv");
-    moment.relativeTimeThreshold("s", 59);
-    moment.relativeTimeThreshold("m", 59);
-    moment.relativeTimeThreshold("h", 23);
-
-    /*
-     * Använd AJAX för att fråga servern efter filen benämnd TILLFALLE i föregående mapp
-     * relativt till denna javascript fil.
-     */
-    $.ajax({
-        url: "../tillfalle",
-        success: function(json) {
-            data = $.parseJSON(json);
-        }
-    }).done(function() {
-        done_fn();
-    });
+	/** 
+	 * Använd AJAX för att fråga servern efter filen tillfalle.json i föregående mapp
+	 * relativt till denna javascript fil.
+	 */
+	$.ajax({
+		url: "../tillfalle.json",
+		dataType: "json",
+		success: function(json) {
+			done_fn(json);
+		}
+	});
 });
